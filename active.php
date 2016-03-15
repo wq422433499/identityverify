6c8d4326cc7eb0147943ad070296d030442ae424
BrownWu <wq422433499@hotmail.com>
2016-03-15 02:28:03 -0400

    Update active.php

--- active.php
+++ active.php
@@ -7,7 +7,7 @@
 <?php
 
 //连接数据库
-$con = mysql_connect("localhost","********","*******");
+$con = mysql_connect("localhost","******","******");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
@@ -35,23 +35,26 @@ $query = mysql_query("select verifitoken_exptime from pre_common_member_profile 
 $row = mysql_fetch_assoc($query);//得到数据
 if($row){         //如果有数据存在
 	if($nowtime>$row['verifitoken_exptime']){ //30min,如果已经过期
-		$msg = '您的激活有效期已过，请登录您的帐号重新发送激活邮件.';
+	
+		echo "<script> alert('您的激活码有效期已过，请重新申请发送激活邮件.');location.href='http://forum.baystgroup.ca/identityverify/'; </script>"; //过期提示消息
+		
 	}else{  //没有过期
 	
 	mysql_query("UPDATE pre_common_member SET extgroupids=concat(extgroupids,'	','$identity') where uid='$uid'");//修改数据库数据
 	$rc = mysql_affected_rows();
 if($rc>0){  //成功
-mysql_query("UPDATE `pre_common_member_profile` SET verifitoken=null,verifitoken_exptime=null,verifitime=null, identity=null,temporaryemail=null WHERE `uid`= '$uid'");//清空临时数据库
-mysql_query("update pre_common_member set verify_email=concat(verify_email,'$email') where uid='$uid'");//更新用户邮箱
-    $msg = '验证成功';
-    } else {$msg = '验证失败，请联系网站管理员';} //验证失败
+	mysql_query("UPDATE `pre_common_member_profile` SET verifitoken=null,verifitoken_exptime=null,verifitime=null, identity=null,temporaryemail=null WHERE `uid`= '$uid'");//清空临时数据库
+	mysql_query("update pre_common_member set verify_email=concat(verify_email,'$email') where uid='$uid'");//更新用户邮箱
+
+	echo "<script> alert('恭喜您，验证成功');location.href='http://forum.baystgroup.ca/forum.php'; </script>"; //弹出提示消息
+    
+    } echo "<script> alert('验证失败，请联系网站管理员.');location.href='http://forum.baystgroup.ca/forum.php?mod=forumdisplay&fid=64'; </script>"; //验证失败
 
 	}
 }else{  //其他情况
-	$msg = '未知错误，请联系网站管理员';	
+	echo "<script> alert('验证失败，请联系网站管理员.');location.href='http://forum.baystgroup.ca/forum.php?mod=forumdisplay&fid=64'; </script>";	
 }
 
-echo $msg;
 mysql_close($con);
 
 ?>
